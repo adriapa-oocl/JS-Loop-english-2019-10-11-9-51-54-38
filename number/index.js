d43203f2605c1720d7a02eba2b46e5fada949c83
ADRIAPA <ADRIAPA@OOCL.COM>
2021-07-23 14:52:01 +0800

    completed requirements for multiplication table and number

---
+++ number/index.js
@@ -0,0 +1,9 @@
+for(let x=1; x<=20; x++){
+    if(x % 2 != 0){
+        document.write(x+" is odd number");
+    }
+    else{
+        document.write(x+" is even number");
+    }
+    document.write('<br>');
+}
